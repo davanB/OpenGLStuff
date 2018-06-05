f9d1110a958023725c4fc4d48d0b3f9cc4dc32be
Davan Basran <davan.basran@gmail.com>
2018-06-04 22:52:49 -0400

    finish making simple black box

--- openGLTUT/main.cpp
+++ openGLTUT/main.cpp
@@ -14,9 +14,54 @@
 //GLFW
 #include <GLFW/glfw3.h>
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (glfwInit() != GL_TRUE) {
+        std::cout << "GLFW Failed to init" << std::endl;
+        return 1;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Davan's Window", NULL, NULL);
+    if (!window) {
+        std::cout << "failed to create glfw window" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to init GLAD" << std::endl;
+        return 1;
+    }
+    
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
